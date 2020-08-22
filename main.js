939db97263a8a3155ea91e6ea7bf26c29a87eb7d
Katsiantonis_Ilias <61645161+Katsiantonis@users.noreply.github.com>
2020-08-22 22:23:04 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(".search_btn").click(function () {
+        $(".search_box").toggleClass("move");
+        $(".search_text").toggleClass("text_animation");
+        $(".search_btn").toggleClass("color");
+        $(".image").toggleClass("opacity");
+        $(".image").toggleClass("img_move");
+        $(".text").toggleClass("text_move");
+    });
+});
